ca0bf0f8e7f8382d05ecfdf9a54df371843c9657
Deen17 <adilriazudeen@gmail.com>
2019-11-03 01:02:03 -0400

    redis initial state

--- pyDLT/initial_state.py
+++ pyDLT/initial_state.py
@@ -7,12 +7,16 @@ async def setInitialState():
                          port=6379,
                          db=0)
     await client.flushdb()
-    await client.incr('userid', 1)
+    await client.incr('transactionid', 1)
     user1dict = {
         "name": "A",
         "accNo": "000001",
         "routingNo": "000001",
         "balance": "100",
+    }
+    user1balance = {
+        "name": user1dict["name"],
+        "balance": user1dict["balance"]
     }
     user2dict = {
         "name": "B",
@@ -20,7 +24,14 @@ async def setInitialState():
         "routingNo": "000002",
         "balance": "100",
     }
-    await client.hmset()
+    user2balance = {
+        "name": user2dict["name"],
+        "balance": user2dict["balance"]
+    }
+    await client.hmset(user1dict["routingNo"] + user1dict["accNo"],
+                       user1balance)
+    await client.hmset(user2dict["routingNo"] + user2dict["accNo"],
+                       user2balance)
 
 
 loop = asyncio.get_event_loop()
