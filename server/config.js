e46c1e462e48c7da7f16233b81402f4720da12a3
Deen17 <adilriazudeen@gmail.com>
2019-11-14 14:37:44 -0500

    Revert "Merge branch 'adil_consolidate' into abdul"
    This reverts commit bd4fc90c4878876be3dd27664d22e25d93a3bb72, reversing
    changes made to c24bf53bb0f5a0bf3a4e286e3f836b57e5c46599.

--- server/config.js
+++ server/config.js
@@ -3,13 +3,5 @@ module.exports={
     appServer: {
         port: 3000,
         hostname: 'localhost'
-    },
-    credentials: {
-        passphrase: 'jpmdlt'
-    },
-    redisOptions: {
-        port: 6379,
-        ip: '104.196.105.254',
-    },
-    
+    }
 }
